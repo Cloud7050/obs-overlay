0e653febc0c01aabda999916fcb153c70ab9645e
Cloud <44526554+Cloud7050@users.noreply.github.com>
2026-06-05 01:41:17 +0800

    Add supporting files

---
+++ README.md
@@ -0,0 +1,3 @@
+My custom OBS input overlay via <https://github.com/univrsal/input-overlay> & <https://univrsal.github.io/input-overlay/cct>.
+
+![](./Overlay.png)
---
+++ link.bat
@@ -0,0 +1,2 @@
+mklink "C:\Users\cloud\Documents\Cache\Recordings\Input Overlay\Cloud\overlay.png" "C:\Users\cloud\Documents\Cache\Art\Aseprite\Input Overlay\Overlay.png"
+mklink "C:\Users\cloud\Documents\Cache\Recordings\Input Overlay\Cloud\overlay.json" "C:\Users\cloud\Documents\Cache\Art\Aseprite\Input Overlay\overlay.json"
